bc9a0d377948e06a33521e8ea110e67d52fa25ad
alexander <sashanight@yandex.ru>
2018-09-27 20:04:37 +0300

    bug fixes

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,13 +1,7 @@
 """It is module of tests."""
 from hangman import choose_the_word
-from hangman import create_bag_of_vocabulary_words
 
 
-def test_choose_the_word_function():
+def test_hello():
     """Test the correctness of choose_the_word function."""
     assert isinstance(choose_the_word(["hello", "word"]), str)
-
-
-def test_create_bag_of_vocabulary_words_function():
-    """Test the correctness of create_bag_of_vocabulary_words function."""
-    assert create_bag_of_vocabulary_words()
